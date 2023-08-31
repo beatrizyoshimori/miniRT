a85acce60fa00a2eef1aea050824c41163e4c453
Beatriz Yoshimori Da Silva <byoshimo@c3r5p3.42sp.org.br>
2023-08-30 22:09:48 -0300

    updated functions

--- minirt/sources/parser/validate_data.c
+++ minirt/sources/parser/validate_data.c
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 21:19:14 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/08/23 19:10:45 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/08/30 21:59:24 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,21 +42,11 @@ int	validate_coordinates(char **coordinates)
 
 int	validate_normalized_vector(t_coordinates coordinates)
 {
-	double	norm;
-	double	aux;
-
-	aux = pow(coordinates.x, 2) + pow(coordinates.y, 2) + pow(coordinates.z, 2);
-	norm = sqrt(aux);
-	norm = round(norm * pow(10, 6)) / pow(10, 6);
-	if (norm == 1)
-		return (1);
-	return (0);
+	return (are_equals(get_vector_magnitude(coordinates), 1));
 }
 
 int	validate_angle(char *angle)
 {
-	if (angle && validate_int(angle) && ft_atoi(angle) >= 0 \
-		&& ft_atoi(angle) <= 180)
-		return (1);
-	return (0);
+	return (angle && validate_int(angle) && ft_atoi(angle) >= 0 \
+		&& ft_atoi(angle) <= 180);
 }
