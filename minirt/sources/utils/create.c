c3e703e27a563108a54f2cc4d3701d9a2de8e095
Beatriz Yoshimori Da Silva <byoshimo@c3r5p3.42sp.org.br>
2023-08-30 22:12:58 -0300

    updated functions

--- minirt/sources/utils/create.c
+++ minirt/sources/utils/create.c
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 18:42:32 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/08/30 18:50:14 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/08/30 22:11:52 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 t_coordinates	create_point(double x, double y, double z)
 {
-	return ((t_coordinates){.x = x, .y = y, .z = z, .w = 1});
+	return ((t_coordinates){x, y, z, 1});
 }
 
 t_coordinates	create_vector(double x, double y, double z)
 {
-	return ((t_coordinates){.x = x, .y = y, .z = z, .w = 0});
+	return ((t_coordinates){x, y, z, 0});
 }
