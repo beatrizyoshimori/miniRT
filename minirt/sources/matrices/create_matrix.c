ab50cceafaa298604ede984ec69dc3b7c784b0db
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-02 21:28:55 -0300

    created create_matrix, create_identity_matrix, transpose_matrix and invert_matrix functions

--- minirt/sources/matrices/create_matrix.c
+++ minirt/sources/matrices/create_matrix.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 15:20:27 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/09/02 15:41:02 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/02 21:21:03 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,60 @@ double	**create_matrix(int size)
 		i++;
 	}
 	return (matrix);
+}
+
+double	**create_identity_matrix(int size)
+{
+	int		i;
+	double	**matrix;
+
+	matrix = create_matrix(size);
+	i = 0;
+	while (i < size)
+	{
+		matrix[i][i] = 1;
+		i++;
+	}
+	return (matrix);
+}
+
+double	**transpose_matrix(double **matrix)
+{
+	int		i;
+	int		j;
+	int		size;
+	double	**trans_matrix;
+
+	size = length_matrix(matrix);
+	trans_matrix = create_matrix(size);
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			trans_matrix[j][i] = matrix [i][j];
+			j++;
+		}
+		i++;
+	}
+	return (trans_matrix);
+}
+
+double	**invert_matrix(double **matrix)
+{
+	double	determinant;
+	double	**cofactor;
+	double	**trans_cofactor;
+	double	**inverse;
+
+	cofactor = get_cofactor_matrix(matrix);
+	trans_cofactor = transpose_matrix(cofactor);
+	free_matrix(&cofactor);
+	determinant = get_determinant(matrix);
+	if (!determinant)
+		return (NULL);
+	inverse = multiply_matrix_by_scalar(1 / determinant, trans_cofactor);
+	free_matrix(&trans_cofactor);
+	return (inverse);
 }
