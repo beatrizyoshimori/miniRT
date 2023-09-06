bc326c2b55c7fea8fe1b9f8c325abde4b6a143a5
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-06 16:53:33 -0300

    changed length_matrix to matrix_length

--- test/unit_test/matrices/matrix_length_test.c
+++ test/unit_test/matrices/matrix_length_test.c
@@ -1,60 +1,60 @@
 #include "unit_test.h"
 
-Test(matrices, test_length_matrix_0) {
+Test(matrices, test_matrix_length_0) {
 	int				size = 0;
 	double			**E = create_matrix(size);
 	
-	int				result = length_matrix(E);
+	int				result = matrix_length(E);
 
 	cr_assert(epsilon_eq(flt, size, result, EPSILON));
 	free_matrix(&E);
 }
 
-Test(matrices, test_length_matrix_1) {
+Test(matrices, test_matrix_length_1) {
 	int				size = 1;
 	double			**E = create_matrix(size);
 	
-	int				result = length_matrix(E);
+	int				result = matrix_length(E);
 
 	cr_assert(epsilon_eq(flt, size, result, EPSILON));
 	free_matrix(&E);
 }
 
-Test(matrices, test_length_matrix_2) {
+Test(matrices, test_matrix_length_2) {
 	int				size = 2;
 	double			**E = create_matrix(size);
 	
-	int				result = length_matrix(E);
+	int				result = matrix_length(E);
 
 	cr_assert(epsilon_eq(flt, size, result, EPSILON));
 	free_matrix(&E);
 }
 
-Test(matrices, test_length_matrix_3) {
+Test(matrices, test_matrix_length_3) {
 	int				size = 3;
 	double			**E = create_matrix(size);
 	
-	int				result = length_matrix(E);
+	int				result = matrix_length(E);
 
 	cr_assert(epsilon_eq(flt, size, result, EPSILON));
 	free_matrix(&E);
 }
 
-Test(matrices, test_length_matrix_4) {
+Test(matrices, test_matrix_length_4) {
 	int				size = 4;
 	double			**E = create_matrix(size);
 	
-	int				result = length_matrix(E);
+	int				result = matrix_length(E);
 
 	cr_assert(epsilon_eq(flt, size, result, EPSILON));
 	free_matrix(&E);
 }
 
-Test(matrices, test_length_matrix_5) {
+Test(matrices, test_matrix_length_5) {
 	int				size = 5;
 	double			**E = create_matrix(size);
 	
-	int				result = length_matrix(E);
+	int				result = matrix_length(E);
 
 	cr_assert(epsilon_eq(flt, size, result, EPSILON));
 	free_matrix(&E);
