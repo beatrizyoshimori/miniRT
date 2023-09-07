3c3cd2703438cfe5bc342db7f48fc58dea7fb8b3
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-07 11:58:25 -0300

    inverted functions.

--- minirt/sources/ray_casting/intersections.c
+++ minirt/sources/ray_casting/intersections.c
@@ -6,11 +6,24 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 19:41:48 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/09/06 21:54:19 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/07 11:47:41 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	add_intersection(t_intersections **intersections, t_intersection new)
+{
+	int	i;
+
+	i = 0;
+	while (i < new.count)
+	{
+		intersections_list_add(intersections, \
+			new_intersection(new.object, new.t[i]));
+		i++;
+	}
+}
 
 void	intersections(t_rt *rt, t_ray ray)
 {
@@ -25,18 +38,5 @@ void	intersections(t_rt *rt, t_ray ray)
 			add_intersection(&rt->intersections, intersection);
 		i++;
 	}
-	free_intersections(&rt->intersections);
-}
-
-void	add_intersection(t_intersections **intersections, t_intersection new)
-{
-	int	i;
-
-	i = 0;
-	while (i < new.count)
-	{
-		intersections_list_add(intersections, \
-			new_intersection(new.object, new.t[i]));
-		i++;
-	}
+	// free_intersections(&rt->intersections);
 }
