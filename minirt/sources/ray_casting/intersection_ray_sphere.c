7331f87ea75e10eea26fd870e8fdf5acf43ddba8
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-07 11:57:47 -0300

    changed 1 to radio^2 on c calculation.

--- minirt/sources/ray_casting/intersection_ray_sphere.c
+++ minirt/sources/ray_casting/intersection_ray_sphere.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 21:11:39 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/09/06 21:53:10 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/07 11:23:39 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ t_intersection	calculate_ray_sphere_intersections(t_ray ray, t_sphere sphere)
 	sphere_to_ray = subtract_tuples(ray.point, sphere.point);
 	a = calculate_dot_product(ray.vector, ray.vector);
 	b = 2 * calculate_dot_product(ray.vector, sphere_to_ray);
-	c = calculate_dot_product(sphere_to_ray, sphere_to_ray) - 1;
+	c = calculate_dot_product(sphere_to_ray, sphere_to_ray) \
+		- pow(sphere.diameter / 2, 2);
 	discriminant = calculate_discriminant(a, b, c);
 	if (discriminant < 0)
 		return ((t_intersection){SP, 0, .t = {0}});
