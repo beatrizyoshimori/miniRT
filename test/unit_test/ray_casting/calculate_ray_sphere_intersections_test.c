aaf300687a42b124374a54304d37bb0a0e64da1c
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-07 11:56:14 -0300

    created tests.

--- test/unit_test/ray_casting/calculate_ray_sphere_intersections_test.c
+++ test/unit_test/ray_casting/calculate_ray_sphere_intersections_test.c
@@ -1,7 +1,7 @@
 #include "unit_test.h"
 
 Test(ray_casting, test_calculate_ray_sphere_intersections_0) {
-	t_sphere	sphere = {{0, 0, 0, 1}, 1, {0, 0, 0}};
+	t_sphere	sphere = {{0, 0, 0, 1}, 2, {0, 0, 0}};
 	t_ray	ray = {{0, 0, -5, 1}, {0, 0, 1, 0}};
 
 	t_intersection	intersection = calculate_ray_sphere_intersections(ray, sphere);
@@ -13,7 +13,7 @@ Test(ray_casting, test_calculate_ray_sphere_intersections_0) {
 }
 
 Test(ray_casting, test_calculate_ray_sphere_intersections_1) {
-	t_sphere	sphere = {{0, 0, 0, 1}, 1, {0, 0, 0}};
+	t_sphere	sphere = {{0, 0, 0, 1}, 2, {0, 0, 0}};
 	t_ray	ray = {{0, 1, -5, 1}, {0, 0, 1, 0}};
 
 	t_intersection	intersection = calculate_ray_sphere_intersections(ray, sphere);
@@ -24,7 +24,7 @@ Test(ray_casting, test_calculate_ray_sphere_intersections_1) {
 }
 
 Test(ray_casting, test_calculate_ray_sphere_intersections_2) {
-	t_sphere	sphere = {{0, 0, 0, 1}, 1, {0, 0, 0}};
+	t_sphere	sphere = {{0, 0, 0, 1}, 2, {0, 0, 0}};
 	t_ray	ray = {{0, 0, 0, 1}, {0, 0, 1, 0}};
 
 	t_intersection	intersection = calculate_ray_sphere_intersections(ray, sphere);
@@ -36,7 +36,7 @@ Test(ray_casting, test_calculate_ray_sphere_intersections_2) {
 }
 
 Test(ray_casting, test_calculate_ray_sphere_intersections_3) {
-	t_sphere	sphere = {{0, 0, 0, 1}, 1, {0, 0, 0}};
+	t_sphere	sphere = {{0, 0, 0, 1}, 2, {0, 0, 0}};
 	t_ray	ray = {{0, 0, 2, 1}, {0, 0, 1, 0}};
 
 	t_intersection	intersection = calculate_ray_sphere_intersections(ray, sphere);
@@ -48,11 +48,23 @@ Test(ray_casting, test_calculate_ray_sphere_intersections_3) {
 }
 
 Test(ray_casting, test_calculate_ray_sphere_intersections_4) {
-	t_sphere	sphere = {{0, 0, 0, 1}, 1, {0, 0, 0}};
+	t_sphere	sphere = {{0, 0, 0, 1}, 2, {0, 0, 0}};
 	t_ray	ray = {{0, 2, 0, 1}, {0, 0, 1, 0}};
 
 	t_intersection	intersection = calculate_ray_sphere_intersections(ray, sphere);
 
 	cr_assert(epsilon_eq(flt, SP, intersection.object, EPSILON));
 	cr_assert(epsilon_eq(flt, 0, intersection.count, EPSILON));
+}
+
+Test(ray_casting, test_calculate_ray_sphere_intersections_5) {
+	t_sphere	sphere = {{0, 0, 0, 1}, 4, {0, 0, 0}};
+	t_ray	ray = {{0, 0, -5, 1}, {0, 0, 1, 0}};
+
+	t_intersection	intersection = calculate_ray_sphere_intersections(ray, sphere);
+
+	cr_assert(epsilon_eq(flt, SP, intersection.object, EPSILON));
+	cr_assert(epsilon_eq(flt, 2, intersection.count, EPSILON));
+	cr_assert(epsilon_eq(flt, 3, intersection.t[0], EPSILON));
+	cr_assert(epsilon_eq(flt, 7, intersection.t[1], EPSILON));
 }
