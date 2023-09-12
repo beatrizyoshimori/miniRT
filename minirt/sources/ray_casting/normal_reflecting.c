59cf9793aec6170723f97e43a0bc072db11917fd
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-11 21:30:18 -0300

    changed function to calculate_normal and added cylinder calculation

--- minirt/sources/ray_casting/normal_reflecting.c
+++ minirt/sources/ray_casting/normal_reflecting.c
@@ -3,20 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   normal_reflecting.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 14:02:29 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/09/08 18:22:27 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/09/11 20:12:40 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_coordinates	calculate_sphere_normal(t_intersections *hit)
+t_coordinates	calculate_normal(t_intersections *hit)
 {
 	t_coordinates	vector;
 
-	vector = subtract_tuples(hit->hit_point, hit->sphere->point);
+	if (hit->type == SP)
+		vector = subtract_tuples(hit->hit_point, hit->sphere->point);
+	else if (hit->type == CY)
+		vector = subtract_tuples(hit->hit_point, hit->cylinder->point);
 	vector.w = 0;
 	return (normalize_vector(vector));
 }
