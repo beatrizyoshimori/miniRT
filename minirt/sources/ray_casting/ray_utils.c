25a31cd1f990a6fa93da1abb37a24381c76095a5
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-13 19:47:27 -0300

    changed create_ray function, creating ray like Jamis.

--- minirt/sources/ray_casting/ray_utils.c
+++ minirt/sources/ray_casting/ray_utils.c
@@ -6,19 +6,25 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 21:45:36 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/09/11 20:05:51 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/12 16:54:06 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_ray	create_ray(t_rt *rt, double x, double y)
+t_ray	create_ray(t_rt *rt, double i, double j)
 {
+	t_coordinates	pixel;
 	t_coordinates	point;
 	t_coordinates	vector;
+	double			x;
+	double			y;
 
-	point = create_point(x, y, -1);
-	vector = rt->camera.vector;
+	x = rt->render.half_width - rt->render.pixel_size * (i + 0.5);
+	y = rt->render.half_height - rt->render.pixel_size * (j + 0.5);
+	pixel = multiply_matrix_tuple(rt->render.inverse, create_point(x, y, -1));
+	point = multiply_matrix_tuple(rt->render.inverse, create_point(0, 0, 0));
+	vector = normalize_vector(subtract_tuples(pixel, point));
 	return ((t_ray){point, vector});
 }
 
