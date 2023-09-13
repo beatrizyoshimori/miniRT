a31e30dfa3cab1403eef8675f79658d64702f037
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-13 19:43:34 -0300

    changed new_intersection function.

--- minirt/sources/ray_casting/intersections.c
+++ minirt/sources/ray_casting/intersections.c
@@ -6,37 +6,22 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 19:41:48 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/09/11 20:16:48 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/13 18:40:43 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-static int	check_position_camera(double t[100], int count)
+void	add_intersection(t_ray ray, t_intersections **intersections, \
+	t_intersection new)
 {
 	int	i;
 
 	i = 0;
-	while (i < count - 1)
-	{
-		if (t[i] < 0 && t[i + 1] > 0)
-			return (INSIDE);
-		i++;
-	}
-	return (OUTSIDE);
-}
-
-void	add_intersection(t_intersections **intersections, t_intersection new)
-{
-	int		i;
-	int		position_camera;
-
-	i = 0;
 	while (i < new.count)
 	{
-		position_camera = check_position_camera(new.t, new.count);
 		intersections_list_add(intersections, \
-			new_intersection(new, i, position_camera));
+			new_intersection(new, i, ray));
 		i++;
 	}
 }
@@ -44,22 +29,22 @@ void	add_intersection(t_intersections **intersections, t_intersection new)
 void	intersections(t_rt *rt, t_ray ray, t_intersections **intersections)
 {
 	int				i;
-	t_intersection	intersection;
+	t_intersection	inter;
 
 	i = 0;
 	while (i < rt->num_sp)
 	{
-		intersection = calculate_ray_sphere_intersections(ray, &rt->spheres[i]);
-		if (intersection.count)
-			add_intersection(intersections, intersection);
+		inter = calculate_ray_sphere_intersections(ray, &rt->spheres[i]);
+		if (inter.count)
+			add_intersection(ray, intersections, inter);
 		i++;
 	}
 	i = 0;
 	while (i < rt->num_cy)
 	{
-		intersection = calculate_ray_cylinder_intersections(ray, &rt->cylinders[i]);
-		if (intersection.count)
-			add_intersection(intersections, intersection);
+		inter = calculate_ray_cylinder_intersections(ray, &rt->cylinders[i]);
+		if (inter.count)
+			add_intersection(ray, intersections, inter);
 		i++;
 	}
 }
@@ -68,7 +53,7 @@ t_intersections	*get_hit(t_intersections *intersections)
 {
 	while (intersections)
 	{
-		if (intersections->t >= 0)
+		if (intersections->t > 0)
 			break ;
 		intersections = intersections->next;
 	}
