94fd5cd4e342bcd679d6d2dc504ac016c7c88264
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-13 18:49:22 -0300

    added init_object_matrix function.

--- minirt/sources/minirt.c
+++ minirt/sources/minirt.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 18:57:17 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/09/11 21:34:55 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/13 18:39:07 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,33 @@ static void	read_rt(t_rt *rt, char *file)
 	free(buffer);
 }
 
+void	init_object_matrix(t_rt *rt)
+{
+	int		i;
+	double	r;
+	double	**scaling;
+	double	**translation;
+	double	**product;
+
+	scaling = NULL;
+	translation = NULL;
+	product = NULL;
+	i = 0;
+	while (i < rt->num_sp)
+	{
+		r = rt->spheres[i].diameter / 2;
+		scaling = create_scaling_matrix(create_point(r, r, r));
+		translation = create_translation_matrix(rt->spheres[i].point);
+		product = multiply_matrices(translation, scaling);
+		rt->spheres[i].inverse = invert_matrix(product);
+		rt->spheres[i].transpose = transpose_matrix(rt->spheres[i].inverse);
+		i++;
+	}
+	free_matrix(&scaling);
+	free_matrix(&translation);
+	free_matrix(&product);
+}
+
 static t_rt	*init_rt(void)
 {
 	t_rt	*rt;
@@ -62,6 +89,7 @@ int	main(int argc, char *argv[])
 	check_arg(argc, argv);
 	read_rt(rt, argv[1]);
 	parser(rt);
+	init_object_matrix(rt);
 	render(rt);
 	return (0);
 }
