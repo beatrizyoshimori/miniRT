819690463e80dbaef21950b4280c0ef88aff4d67
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-13 19:46:43 -0300

    changed calculate_normal function, calculating it like Jamis.

--- minirt/sources/ray_casting/normal_reflecting.c
+++ minirt/sources/ray_casting/normal_reflecting.c
@@ -6,22 +6,26 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 14:02:29 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/09/11 20:12:40 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/12 21:16:28 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_coordinates	calculate_normal(t_intersections *hit)
+void	calculate_normal(t_intersections *hit)
 {
-	t_coordinates	vector;
+	t_coordinates	o_point;
+	t_coordinates	o_normal;
+	t_coordinates	w_normal;
 
 	if (hit->type == SP)
-		vector = subtract_tuples(hit->hit_point, hit->sphere->point);
-	else if (hit->type == CY)
-		vector = subtract_tuples(hit->hit_point, hit->cylinder->point);
-	vector.w = 0;
-	return (normalize_vector(vector));
+	{
+		o_point = multiply_matrix_tuple(hit->sphere->inverse, hit->hit_point);
+		o_normal = subtract_tuples(o_point, create_point(0, 0, 0));
+		w_normal = multiply_matrix_tuple(hit->sphere->transpose, o_normal);
+	}
+	w_normal.w = 0;
+	hit->normal = normalize_vector(w_normal);
 }
 
 t_coordinates	calculate_reflecting_vector(t_coordinates light, \
