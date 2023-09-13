7c8423950938838eca12f161297fa39ed81fe59c
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-13 19:02:10 -0300

    multiplied ray by inverse of object transformation matrix.

--- minirt/sources/ray_casting/intersection_ray_object.c
+++ minirt/sources/ray_casting/intersection_ray_object.c
@@ -6,45 +6,49 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 21:11:39 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/09/11 20:04:26 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/13 18:52:16 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_intersection	calculate_ray_sphere_intersections(t_ray ray, t_sphere *sphere)
+t_intersection	calculate_ray_sphere_intersections(t_ray ray_in, \
+	t_sphere *sphere)
 {
 	t_intersection	inter;
 	t_discriminant	disc;
+	t_ray			ray;
 
+	ray = transform_ray(ray_in, sphere->inverse);
 	inter = (t_intersection){SP, 0, .sphere = sphere};
-	disc = calculate_discriminant_ray_sphere(ray, sphere);
+	disc = calculate_discriminant_ray_sphere(ray);
 	if (disc.discriminant < 0)
 		return (inter);
 	if (disc.discriminant == 0)
 	{
 		inter.count = 1;
 		inter.t[0] = (-disc.b - sqrt(disc.discriminant)) / (2 * disc.a);
-		inter.hit_point[0] = calculate_ray_position(ray, inter.t[0]);
+		inter.hit_point[0] = calculate_ray_position(ray_in, inter.t[0]);
 	}
 	else
 	{
 		inter.count = 2;
 		inter.t[0] = (-disc.b - sqrt(disc.discriminant)) / (2 * disc.a);
 		inter.t[1] = (-disc.b + sqrt(disc.discriminant)) / (2 * disc.a);
-		inter.hit_point[0] = calculate_ray_position(ray, inter.t[0]);
-		inter.hit_point[1] = calculate_ray_position(ray, inter.t[1]);
+		inter.hit_point[0] = calculate_ray_position(ray_in, inter.t[0]);
+		inter.hit_point[1] = calculate_ray_position(ray_in, inter.t[1]);
 	}
 	return (inter);
 }
 
-t_intersection	calculate_ray_cylinder_intersections(t_ray ray, t_cylinder *cylinder)
+t_intersection	calculate_ray_cylinder_intersections(t_ray ray, \
+	t_cylinder *cylinder)
 {
 	t_intersection	inter;
 	t_discriminant	disc;
 
 	inter = (t_intersection){CY, 0, .cylinder = cylinder};
-	disc = calculate_discriminant_ray_cylinder(ray, cylinder);
+	disc = calculate_discriminant_ray_cylinder(ray);
 	if (disc.discriminant < 0)
 		return (inter);
 	if (disc.discriminant == 0)
