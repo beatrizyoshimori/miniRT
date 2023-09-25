89f0d60441d6ed807f72a414af821563a8492ffa
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-25 14:43:47 -0300

    calculated cylinder and plane normal

--- minirt/sources/ray_casting/normal_reflecting.c
+++ minirt/sources/ray_casting/normal_reflecting.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 14:02:29 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/09/15 19:59:37 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/25 14:07:58 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,14 @@ void	calculate_normal(t_intersections *hit)
 		o_point = multiply_matrix_tuple(hit->sphere->inverse, hit->hit_point);
 		o_normal = subtract_tuples(o_point, create_point(0, 0, 0));
 		w_normal = multiply_matrix_tuple(hit->sphere->transpose, o_normal);
+	}
+	else if (hit->type == PL)
+		w_normal = hit->plane->vector;
+	else if (hit->type == CY)
+	{
+		o_point = multiply_matrix_tuple(hit->cylinder->inverse, hit->hit_point);
+		o_normal = subtract_tuples(o_point, create_point(0, o_point.y, 0));
+		w_normal = multiply_matrix_tuple(hit->cylinder->transpose, o_normal);
 	}
 	w_normal.w = 0;
 	hit->normal = normalize_vector(w_normal);
