7ff279dd3923770d028bfc49d3e69acf1a1bd461
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-25 14:42:46 -0300

    refactored functions

--- minirt/sources/ray_casting/intersections.c
+++ minirt/sources/ray_casting/intersections.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 19:41:48 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/09/15 19:58:17 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/25 14:35:40 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ static void	add_intersection(t_ray ray, t_intersections **intersections, \
 	}
 }
 
-void	intersections(t_rt *rt, t_ray ray, t_intersections **intersections)
+static void	intersections_spheres_planes(t_rt *rt, t_ray ray, \
+			t_intersections **intersections)
 {
 	int				i;
 	t_intersection	inter;
@@ -40,6 +41,22 @@ void	intersections(t_rt *rt, t_ray ray, t_intersections **intersections)
 		i++;
 	}
 	i = 0;
+	while (i < rt->num_pl)
+	{
+		inter = calculate_ray_plane_intersections(ray, &rt->planes[i]);
+		if (inter.count)
+			add_intersection(ray, intersections, inter);
+		i++;
+	}
+}
+
+static void	intersections_cylinders(t_rt *rt, t_ray ray, \
+			t_intersections **intersections)
+{
+	int				i;
+	t_intersection	inter;
+
+	i = 0;
 	while (i < rt->num_cy)
 	{
 		inter = calculate_ray_cylinder_intersections(ray, &rt->cylinders[i]);
@@ -47,6 +64,12 @@ void	intersections(t_rt *rt, t_ray ray, t_intersections **intersections)
 			add_intersection(ray, intersections, inter);
 		i++;
 	}
+}
+
+void	intersections(t_rt *rt, t_ray ray, t_intersections **intersections)
+{
+	intersections_spheres_planes(rt, ray, intersections);
+	intersections_cylinders(rt, ray, intersections);
 }
 
 t_intersections	*get_hit(t_intersections *intersections)
