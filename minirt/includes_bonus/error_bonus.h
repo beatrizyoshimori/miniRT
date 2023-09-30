e98d1efa0e64409844d2892a3d0217673cc872d5
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-09-29 22:26:55 -0300

    changed object color error message

--- minirt/includes_bonus/error_bonus.h
+++ minirt/includes_bonus/error_bonus.h
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 16:04:27 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/09/29 18:45:09 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/09/29 19:31:44 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,12 @@
 # define CENTER_SP "Invalid sphere center coordinates."
 # define D_DIAM_SP "Invalid sphere diameter. Diameter is not a double."
 # define R_DIAM_SP "Invalid sphere diameter. Diameter isn't greater than 0."
-# define COLOR_SP "Invalid sphere color."
+# define COLOR_SP "Invalid sphere color or checkerboard/texture parameter."
 # define ARG_PL "Invalid number of arguments on plane."
 # define POINT_PL "Invalid plane point."
 # define NORMAL_PL "Invalid plane normal."
 # define N_NORMAL_PL "Invalid plane normal. Normal is not normalized."
-# define COLOR_PL "Invalid plane color."
+# define COLOR_PL "Invalid plane color or checkerboard/texture parameter."
 # define ARG_CY "Invalid number of arguments on cylinder."
 # define CENTER_CY "Invalid cylinder center coordinates."
 # define VECTOR_CY "Invalid cylinder vector."
@@ -53,14 +53,14 @@
 # define R_DIAM_CY "Invalid cylinder diameter. Diameter isn't greater than 0."
 # define D_HEIGHT_CY "Invalid cylinder height. Height is not a double."
 # define R_HEIGHT_CY "Invalid cylinder height. Height isn't greater than 0."
-# define COLOR_CY "Invalid cylinder color."
+# define COLOR_CY "Invalid cylinder color or checkerboard/texture parameter."
 # define ARG_CO "Invalid number of arguments on cone."
 # define CENTER_CO "Invalid cone center coordinates."
 # define VECTOR_CO "Invalid cone vector."
 # define N_VECTOR_CO "Invalid cone vector. Vector is not normalized."
 # define D_HEIGHT_CO "Invalid cone height. Height is not a double."
 # define R_HEIGHT_CO "Invalid cone height. Height isn't greater than 0."
-# define COLOR_CO "Invalid cone color."
+# define COLOR_CO "Invalid cone color or checkerboard/texture parameter."
 # define MLX_INIT "Could not init window."
 # define MLX_IMAGE "Could not create image."
 # define MLX_IMAGE_TO_WIN "Could not put image to window."
