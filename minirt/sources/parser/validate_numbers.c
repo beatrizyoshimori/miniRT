e569ca7f51bff92eef82b7d3fe184bae41b8338b
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-10-10 14:59:20 -0300

    double started . is not valid anymore.

--- minirt/sources/parser/validate_numbers.c
+++ minirt/sources/parser/validate_numbers.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 21:17:13 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/08/23 18:00:27 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/10/10 14:55:18 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	validate_double(char *number)
 		return (0);
 	if (number[i] == '+' || number[i] == '-')
 		i++;
-	if (!number[i])
+	if (!number[i] || number[i] == '.')
 		return (0);
 	while (ft_isdigit(number[i]))
 		i++;
